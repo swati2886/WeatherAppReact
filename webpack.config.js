549e6c43e2b43a30e6b3768cc64145dc286a7fcd
swati2886 <swati2886@gmail.com>
2016-11-23 22:50:06 +0530

    Add files via upload
    Getting run time exception.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+	      query: {
+		      presets: ['react']
+		    }
+      }
+    ]
+  }
+};
